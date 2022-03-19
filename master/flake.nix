738b75a5d913b1ced55996f4f16cf291248e7ecb
hugosenari <hugosenari@gmail.com>
2022-03-19 03:57:35 -0300

    chore: re-index ../nimpkgs/i/imap/master

--- master/flake.nix
+++ master/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-imap-master.flake = false;
   inputs.src-imap-master.owner = "~ehmry";
-  inputs.src-imap-master.ref   = "refs/heads/master";
+  inputs.src-imap-master.ref   = "master";
   inputs.src-imap-master.repo  = "nim_imap";
   inputs.src-imap-master.type  = "sourcehut";
   
